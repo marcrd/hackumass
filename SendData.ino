f39a9708ae320f604a92830edd350b385817ce00
Marc Rudkowski <coolrudski@gmail.com>
2014-04-06 14:56:36 -0400

    Hack UMass Arduino files

---
+++ SendData.ino
@@ -0,0 +1,25 @@
+const int xPin = 4;
+const int yPin = 5;
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    int xValue;
+    int yValue;
+    xValue = analogRead(xPin);
+    yValue = analogRead(yPin);
+    
+    //Serial.println(yValue);
+    
+    Serial.print(xValue);
+    Serial.print(",");
+    Serial.print(yValue);
+    Serial.println("");
+    
+        
+    delay(100);
+}
---
+++ SerialData.ino
@@ -0,0 +1,25 @@
+#include <Wire.h>
+#include <LSM303.h>
+
+LSM303 compass;
+
+char report[80];
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
+}
+
+void loop()
+{
+  compass.read();
+
+  snprintf(report, sizeof(report), "%6d",
+    compass.a.x);
+  Serial.println(report);
+
+  delay(100);
+}
